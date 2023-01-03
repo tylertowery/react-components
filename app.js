219fc541f7bb5c9a6b4bf31d3abc53eadea5e815
Tyler Towery <tylertowery21@gmail.com>
2023-01-03 11:24:32 -0800

    all bare minimum requirements are complete

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // TODO
+const GroceryList = () => {
+  return (
+    <div>
+      <h2>My Grocery List</h2>
+      <List list={['milk', 'eggs', 'cheese', 'wine', 'bread']}/>
+    </div>
+  )
+}
+
+const { useState } = React;
+
+const ListItem = props => {
+  const [isBold, setBold] = useState(false)
+  const [isGreen, setColor] = useState(false)
+
+  const style = {
+    fontWeight: isBold ? 'bold' : 'normal',
+    color: isGreen ? 'green' : 'black'
+  }
+
+  return (
+    <li style={style} onMouseEnter={() => setBold(true)} onMouseLeave={() => setBold(false)} onClick={() => setColor(!isGreen)}>
+      {props.list}
+    </li>
+  )
+}
+
+const List = props => {
+
+  const onListItemClick = (event) => {
+    console.log(event.target.innerHTML + ' got clicked!');
+  }
+
+  return (
+    <ul onClick={onListItemClick}>
+      {props.list.map((list) => (
+        <ListItem list={list} />
+      ))}
+    </ul>
+  )
+}
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
